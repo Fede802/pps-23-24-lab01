e09078ca80a6be98f4e66a38137bef4956ce6974
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-02-28 12:25:41 +0100

    isInitiallyEmpty test passed

--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,17 +1,21 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import tdd.CircularList;
+import tdd.CircularListImpl;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
-
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @Test
+    void isInitiallyEmpty(){
+        CircularList circularList = new CircularListImpl();
+        assertTrue(circularList.isEmpty());
     }
+
 
 }
